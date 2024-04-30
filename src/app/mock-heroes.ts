e0deb20e06992c27314e225d220d37d8f1de8c64
fmarelliCenit <fmarelli@cenitcon.com>
2024-04-30 12:52:43 +0100

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Kaido' },
+  { id: 13, name: 'Mugiwara no Luffy' },
+  { id: 14, name: 'Big Mom' },
+  { id: 15, name: 'Akagami no Shanks' },
+  { id: 16, name: 'Fujitora' },
+  { id: 17, name: 'Roronoa Zoro' },
+  { id: 18, name: 'Garp' },
+  { id: 19, name: 'Sabo' },
+  { id: 20, name: 'Crocodile' }
+];
