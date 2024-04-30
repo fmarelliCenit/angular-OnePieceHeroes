a85d8976eec8e9a6e7def66d01b14345de2442d2
fmarelliCenit <fmarelli@cenitcon.com>
2024-04-30 13:57:41 +0100

    ended

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Kaido' },
-  { id: 13, name: 'Mugiwara no Luffy' },
-  { id: 14, name: 'Big Mom' },
-  { id: 15, name: 'Akagami no Shanks' },
-  { id: 16, name: 'Fujitora' },
-  { id: 17, name: 'Roronoa Zoro' },
-  { id: 18, name: 'Garp' },
-  { id: 19, name: 'Sabo' },
-  { id: 20, name: 'Crocodile' }
+  { id: 12, name: 'Kaido',desc: ''},
+  { id: 13, name: 'Mugiwara no Luffy',desc: '' },
+  { id: 14, name: 'Big Mom',desc: '' },
+  { id: 15, name: 'Akagami no Shanks',desc: '' },
+  { id: 16, name: 'Fujitora',desc: '' },
+  { id: 17, name: 'Roronoa Zoro',desc: '' },
+  { id: 18, name: 'Garp' ,desc: ''},
+  { id: 19, name: 'Sabo' ,desc: ''},
+  { id: 20, name: 'Crocodile' ,desc: ''}
 ];
